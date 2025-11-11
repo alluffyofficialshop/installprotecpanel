f05a412e74b0918f14a92135664f9a5b738cd846
alluffyofficialshop <alluffyrekber@gmail.com>
2025-11-12 00:33:14 +0700

    Update alluffyprotect5.sh

--- alluffyprotect5.sh
+++ alluffyprotect5.sh
@@ -52,7 +52,7 @@ class NestController extends Controller
         // 🔒 Proteksi: hanya user ID 1 (superadmin) yang bisa akses menu Nest
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests. ©𝗟𝗶𝗴𝗵𝘁𝗦𝗲𝗰𝗿𝗲𝘁 𝗣𝗿𝗼𝘁𝗲𝗰𝘁 t.me/lightsecrett 𝗩𝟭.𝟯');
+            abort(403, '🚫 Akses ditolak! Hanya admin utama (ID 1) yang bisa membuka menu Nests. ©𝗣𝗥𝗢𝗧𝗘𝗖𝗧 𝗕𝗬 𝗔𝗟 𝗟𝗨𝗙𝗙𝗬 t.me/alluffystore 𝗩𝟭.𝟯');
         }
 
         return $this->view->make('admin.nests.index', [
